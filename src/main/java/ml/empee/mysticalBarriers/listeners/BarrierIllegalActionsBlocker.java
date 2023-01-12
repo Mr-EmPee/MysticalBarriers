e5397d53fbd11dd82187745aff80c9b24938dbe8
manuel.palumbo <manuel.palumbo@email.com>
2023-01-12 16:24:24 +0100

    Fix: Compatibility with 1.13

--- src/main/java/ml/empee/mysticalBarriers/listeners/BarrierIllegalActionsBlocker.java
+++ src/main/java/ml/empee/mysticalBarriers/listeners/BarrierIllegalActionsBlocker.java
@@ -2,17 +2,14 @@ package ml.empee.mysticalBarriers.listeners;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
 import ml.empee.ioc.annotations.Bean;
 import ml.empee.mysticalBarriers.MysticalBarriersPlugin;
 import ml.empee.mysticalBarriers.config.Config;
 import ml.empee.mysticalBarriers.model.Barrier;
 import ml.empee.mysticalBarriers.services.BarriersService;
 import ml.empee.mysticalBarriers.utils.LocationUtils;
-import ml.empee.mysticalBarriers.utils.MCLogger;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -23,7 +20,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.LingeringPotionSplashEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
-import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -44,11 +40,11 @@ public class BarrierIllegalActionsBlocker implements Listener {
     this.config = config;
 
     Bukkit.getScheduler().runTaskTimer(plugin, () -> {
-      followedEntities.entrySet().removeIf(entry -> checkFollowedEntity(entry.getKey(), entry.getValue()));
+      followedEntities.entrySet().removeIf(entry -> reflectProjectileOnBarrierTouch(entry.getKey(), entry.getValue()));
     }, 0, 1);
   }
 
-  private boolean checkFollowedEntity(Entity entity, Player shooter) {
+  private boolean reflectProjectileOnBarrierTouch(Entity entity, Player shooter) {
     if (!entity.isValid()) {
       return true;
     } else if (!entity.isOnGround()) {
@@ -85,7 +81,7 @@ public class BarrierIllegalActionsBlocker implements Listener {
   }
 
   @EventHandler
-  public void removeProjectileOnMoveInsideBarrier(ProjectileLaunchEvent event) {
+  public void onProjectileLaunch(ProjectileLaunchEvent event) {
     if (!config.isProjectileMovementBlocked()) {
       return;
     }
@@ -103,8 +99,11 @@ public class BarrierIllegalActionsBlocker implements Listener {
   }
 
   @EventHandler
-  public void blockOnDropInsideBarrier(PlayerDropItemEvent event) {
-    followedEntities.put(event.getItemDrop(), event.getPlayer());
+  public void onDropNearBarrier(PlayerDropItemEvent event) {
+    List<Barrier> barriers = barriersService.findBarriersWithinRangeAt(event.getItemDrop().getLocation(), 8);
+    if (!barriers.isEmpty() && barriers.stream().anyMatch(b -> !b.isHiddenFor(event.getPlayer()))) {
+      followedEntities.put(event.getItemDrop(), event.getPlayer());
+    }
   }
 
   @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
@@ -128,7 +127,7 @@ public class BarrierIllegalActionsBlocker implements Listener {
   }
 
   @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
-  public void cancelOnChorusTeleportation(PlayerTeleportEvent event) {
+  public void cancelOnChorusTeleportationNearBarrier(PlayerTeleportEvent event) {
     if (!config.getBlockChorusFruitTeleportation() ||
         !event.getCause().name().equals("CHORUS_FRUIT")) {
       return;
@@ -151,27 +150,22 @@ public class BarrierIllegalActionsBlocker implements Listener {
   }
 
   @EventHandler
-  public void cancelOnProjectileHit(ProjectileHitEvent event) {
-    if (!(event.getEntity().getShooter() instanceof Player)) {
+  public void cancelOnProjectileDamageInsideBarrier(EntityDamageByEntityEvent event) {
+    if(!(event.getDamager() instanceof Projectile)) {
       return;
     }
 
-    Barrier barrier;
-    if (event.getHitEntity() != null) {
-      barrier = barriersService.findBarrierAt(
-          event.getHitEntity().getLocation().getBlock().getLocation()
-      );
-    } else if (event.getHitBlock() != null) {
-      barrier = LocationUtils.forEachAdjacentBlock(event.getHitBlock().getLocation())
-          .map(barriersService::findBarrierAt)
-          .filter(Objects::nonNull).findFirst().orElse(null);
-    } else {
-      barrier = barriersService.findBarrierAt(event.getEntity().getLocation());
+    Projectile projectile = (Projectile) event.getDamager();
+    if(!(projectile.getShooter() instanceof Player)) {
+      return;
     }
 
-    if (barrier != null && !barrier.isHiddenFor((Player) event.getEntity().getShooter())) {
-      event.setCancelled(true);
+    Barrier barrier = barriersService.findBarrierAt(event.getEntity().getLocation());
+    if(barrier == null || barrier.isHiddenFor((Player) projectile.getShooter())) {
+      return;
     }
+
+    event.setCancelled(true);
   }
 
   @EventHandler
