a786bbb275e2c7b9a22521423ff7c2718ffd893f
Manuel Palumbo <manuel.palumbo@email.com>
2023-04-04 15:21:55 +0200

    Improvement: Barrier break protection

--- src/main/java/ml/empee/mysticalbarriers/handlers/BarrierSpawnHandler.java
+++ src/main/java/ml/empee/mysticalbarriers/handlers/BarrierSpawnHandler.java
@@ -1,17 +1,29 @@
 package ml.empee.mysticalbarriers.handlers;
 
+import com.comphenix.protocol.PacketType.Play.Server;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.WrappedBlockData;
 import io.papermc.paper.event.packet.PlayerChunkLoadEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import ml.empee.ioc.Bean;
 import ml.empee.ioc.RegisteredListener;
 import ml.empee.mysticalbarriers.model.entities.Barrier;
 import ml.empee.mysticalbarriers.services.BarrierService;
 import ml.empee.mysticalbarriers.utils.LocationUtils;
 import ml.empee.mysticalbarriers.utils.PaperUtils;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Handler used to spawn the barrier blocks near a player
@@ -20,8 +32,60 @@ import org.bukkit.event.player.PlayerMoveEvent;
 @RequiredArgsConstructor
 public class BarrierSpawnHandler implements Bean, RegisteredListener {
 
+  private static final ProtocolManager PROTOCOL_MANAGER = ProtocolLibrary.getProtocolManager();
+  private static final JavaPlugin plugin = JavaPlugin.getProvidingPlugin(BarrierSpawnHandler.class);
+  private final PacketAdapter serverBlockChangeListener = new PacketAdapter(plugin, Server.BLOCK_CHANGE) {
+    @Override
+    public void onPacketSending(PacketEvent event) {
+      onServerSendBlockChange(event);
+    }
+  };
+
   private final BarrierService barrierService;
 
+  @Override
+  public void onStart() {
+    PROTOCOL_MANAGER.addPacketListener(serverBlockChangeListener);
+  }
+
+  @Override
+  public void onStop() {
+    PROTOCOL_MANAGER.removePacketListener(serverBlockChangeListener);
+  }
+
+  /**
+   * Re-send barrier blocks when a player breaks it
+   */
+  @SneakyThrows
+  public void onServerSendBlockChange(PacketEvent event) {
+    Player player = event.getPlayer();
+    PacketContainer packet = event.getPacket();
+    Block block = packet.getBlockPositionModifier().read(0).toLocation(player.getWorld()).getBlock();
+    Barrier barrier = barrierService.findBarrierByBlock(block).orElse(null);
+    if (barrier == null || !barrier.isVisibleFor(player)) {
+      return;
+    }
+
+    int distance = LocationUtils.getGreatestAxisDistance(player.getLocation(), block.getLocation());
+    if (distance > barrier.getActivationRange()) {
+      return;
+    }
+
+    Material updateMaterial = packet.getBlockData().read(0).getType();
+    if (!updateMaterial.name().contains("AIR")) {
+      return;
+    }
+
+    packet = packet.deepClone();
+    packet.getBlockData().write(0, WrappedBlockData.createData(barrier.getBlockData()));
+
+    event.setCancelled(true);
+    PROTOCOL_MANAGER.sendServerPacket(player, packet, true);
+  }
+
+  /**
+   * Send barrier blocks when a player move
+   */
   @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
   public void onPlayerMove(PlayerMoveEvent event) {
     if (LocationUtils.isSameBlock(event.getFrom(), event.getTo())) {
@@ -44,10 +108,24 @@ public class BarrierSpawnHandler implements Bean, RegisteredListener {
   }
 
   /**
+   * Prevent players from breaking a barrier block
+   */
+  @EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+  public void onPlayerBreak(BlockBreakEvent event) {
+    Barrier barrier = barrierService.findBarrierByBlock(event.getBlock()).orElse(null);
+    if (barrier == null || !barrier.isVisibleFor(event.getPlayer())) {
+      return;
+    }
+
+    event.setCancelled(true);
+  }
+
+
+  /**
    * Listeners registered only if the server is running paper
    */
   @RequiredArgsConstructor
-  private static class PaperListeners implements Bean, RegisteredListener {
+  public static class PaperListeners implements Bean, RegisteredListener {
     private final BarrierService barrierService;
 
     @Override
@@ -55,6 +133,9 @@ public class BarrierSpawnHandler implements Bean, RegisteredListener {
       return PaperUtils.IS_RUNNING_PAPER;
     }
 
+    /**
+     * Spawn barrier blocks when a player join near a barrier
+     */
     @EventHandler
     public void onPlayerLoadChunk(PlayerChunkLoadEvent event) {
       Player player = event.getPlayer();
