e31bc0b49fa43841768adb31f02753d2edfd02fc
Manuel Palumbo <manuel.palumbo@email.com>
2024-07-27 13:41:32 +0200

    revert feature: removed adventure

--- plugin/utils/build.gradle.kts
+++ plugin/utils/build.gradle.kts
@@ -6,10 +6,6 @@ plugins {
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.15.2-R0.1-SNAPSHOT")
     compileOnly("org.slf4j:slf4j-api:2.0.12")
-
-    api("net.kyori:adventure-platform-bukkit:4.3.3")
-    api("net.kyori:adventure-text-minimessage:4.17.0")
-    api("net.kyori:adventure-text-serializer-legacy:4.17.0")
 }
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(11))
